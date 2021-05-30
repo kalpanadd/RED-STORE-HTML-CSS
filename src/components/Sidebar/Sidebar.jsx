37377b06ace8a7dd1d477a48d6ecad3a8a03f017
kalpana1 <dkdevi.dkd@gmail.com>
2021-05-30 23:58:48 +0530

    sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import './Sidebar.css';
 
+import CreateRoundedIcon from '@material-ui/icons/CreateRounded';
 function Sidebar() {
     return (
         <div className='sidebar'>
+            <div className='sidebar-Header'>
+                <div className='sidebar-info'>
+                    <h2>ProductEngineering</h2>
+                </div>
+                <div className='sidebar-header-icon'>
+                    <CreateRoundedIcon className='MUI-icon' />
+                </div>
+
+            </div>
+
 
         </div>
+
+
     )
 }
 
